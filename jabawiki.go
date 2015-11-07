f207ddc09fda85b01ee35e0b266928722ffa8fa0
Ben Doan <ben@bendoan.me>
2015-11-07 17:39:13 -0600

    moved auth into its own file

--- jabawiki.go
+++ jabawiki.go
@@ -3,23 +3,19 @@ package main
 import (
 	"compress/gzip"
 	"crypto/md5"
-	"crypto/rand"
 	"encoding/csv"
 	"encoding/json"
-	"errors"
 	"flag"
 	"fmt"
 	"github.com/BurntSushi/toml"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/sessions"
 	"github.com/op/go-logging"
-	"golang.org/x/crypto/bcrypt"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"path/filepath"
-	"reflect"
 	"runtime"
 	"strconv"
 	"strings"
@@ -53,8 +49,6 @@ var (
 	store        *sessions.CookieStore
 	articleStore ArticleStore
 	conf         Config
-
-	errUserNotFound = errors.New("User not found in session")
 )
 
 const (
@@ -88,16 +82,17 @@ func BaseHandler(w http.ResponseWriter, r *http.Request) {
 func HandleArticle(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 
-	user, err := getUserFromSession(r)
-	if err != nil {
-		log.Debug("User not found: %v", err)
-		//http.Error(w, "Not allowed", http.StatusUnauthorized)
-		//return
-	}
+	user, _ := getUserFromSession(r)
 
 	vars := mux.Vars(r)
 	title := vars["title"]
 	article, err := articleStore.GetArticle(title)
+
+	if err != nil {
+		log.Debug("Couldn't find article: %v", err)
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
 
 	if isUserAllowed(user, article.Metadata) {
 		switch r.Method {
@@ -112,15 +107,6 @@ func HandleArticle(w http.ResponseWriter, r *http.Request) {
 
 	http.Error(w, "Not allowed", http.StatusUnauthorized)
 	return
-}
-
-func isUserAllowed(user User, articleMetadata ArticleMetadata) bool {
-	hasUser := !reflect.DeepEqual(user, User{})
-	hasArticle := !reflect.DeepEqual(articleMetadata, ArticleMetadata{})
-
-	return hasUser && user.Role == Admin ||
-		hasArticle && articleMetadata.Permission == "public" ||
-		hasUser && hasArticle && articleMetadata.Permission == "registered" && user.Role == Verified
 }
 
 func GetArticle(w http.ResponseWriter, r *http.Request, title string) {
@@ -243,146 +229,6 @@ func writeHistory(w http.ResponseWriter, r *http.Request, article IncomingArticl
 
 	history := fmt.Sprintf("%d | %s | %s\n", time.Now().Unix(), creator, article.Summary)
 	fmt.Fprint(historyFile, history)
-}
-
-func genUUID() string {
-	uuid := make([]byte, 16)
-	n, err := io.ReadFull(rand.Reader, uuid)
-
-	if n != len(uuid) || err != nil {
-		panic(fmt.Sprintf("Couldn't generate uuid %v", err))
-	}
-
-	uuid[8] = uuid[8]&^0xc0 | 0x80
-	uuid[6] = uuid[6]&^0xf0 | 0x40
-	return fmt.Sprintf("%x-%x-%x-%x-%x", uuid[0:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:])
-}
-
-func HandleRegister(w http.ResponseWriter, r *http.Request) {
-	decoder := json.NewDecoder(r.Body)
-	var incomingUser IncomingUser
-	err := decoder.Decode(&incomingUser)
-
-	if err != nil {
-		msg := "Couldn't decode register request data"
-		log.Debug("%s: %v", msg, err)
-		http.Error(w, msg, http.StatusBadRequest)
-		return
-	}
-
-	if _, ok := users[incomingUser.Email]; ok {
-		msg := "Couldn't create account, user already exists"
-		log.Debug("%s: %s", msg, incomingUser.Email)
-		http.Error(w, msg, http.StatusBadRequest)
-		return
-	}
-
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(incomingUser.Password), 10)
-
-	if err != nil {
-		log.Error("Couldn't generate password with bcrypt: %v", err)
-		http.Error(w, INTERNAL_SERVER_ERROR_MSG, http.StatusInternalServerError)
-	}
-
-	usersFile, err := os.OpenFile(filepath.Join(getDataDirPath(), "users.txt"), os.O_RDWR|os.O_APPEND|os.O_CREATE, 0660)
-
-	if err != nil {
-		log.Error("Couldn't open users file: ", err)
-		http.Error(w, INTERNAL_SERVER_ERROR_MSG, http.StatusInternalServerError)
-		return
-	}
-
-	user := User{genUUID(), incomingUser.Email, incomingUser.Name, Unverified, hashedPassword}
-
-	_, err = fmt.Fprintf(usersFile, fmt.Sprintf("%s,%s,%s,%d,%s\n", user.Id, user.Email, user.Name, user.Role, user.Password))
-	if err != nil {
-		log.Error("Couldn't write to users file: %v", err)
-		http.Error(w, INTERNAL_SERVER_ERROR_MSG, http.StatusInternalServerError)
-		return
-	}
-
-	// allow user to be looked up by id or email
-	users[user.Id] = user
-	users[user.Email] = user
-
-	log.Debug("Registered user: %s", user.Email)
-	fmt.Fprint(w, "Good")
-}
-
-func HandleLogin(w http.ResponseWriter, r *http.Request) {
-	decoder := json.NewDecoder(r.Body)
-
-	var incomingUser IncomingUser
-	err := decoder.Decode(&incomingUser)
-
-	if err != nil {
-		msg := "Couldn't decode login request data"
-		log.Debug("%s: %v", msg, err)
-		http.Error(w, msg, http.StatusBadRequest)
-		return
-	}
-
-	if storedUser, ok := users[incomingUser.Email]; ok {
-		if bcrypt.CompareHashAndPassword(storedUser.Password, []byte(incomingUser.Password)) == nil {
-			// login user
-			session, _ := store.Get(r, "user")
-			session.Values["id"] = storedUser.Id
-			session.Save(r, w)
-			fmt.Fprint(w, "Good")
-		} else {
-			log.Debug("Invalid password during login")
-			http.Error(w, "Invalid email or password", http.StatusBadRequest)
-			return
-		}
-	} else {
-		log.Debug("Invalid email during login")
-		http.Error(w, "Invalid email or password", http.StatusBadRequest)
-		return
-	}
-}
-
-func HandleLogout(w http.ResponseWriter, r *http.Request) {
-	session, _ := store.Get(r, "user")
-	session.Values["id"] = -1
-	session.Save(r, w)
-
-	fmt.Fprint(w, "Good")
-}
-
-func getUserFromSession(r *http.Request) (User, error) {
-	session, err := store.Get(r, "user")
-	if err != nil {
-		log.Debug("Couldn't find user: %v", err)
-		return User{}, err
-	}
-
-	if data, ok := session.Values["id"]; ok {
-		if userId, ok := data.(string); ok {
-			if user, ok := users[userId]; ok {
-				return user, nil
-			}
-		}
-	}
-
-	return User{}, errUserNotFound
-}
-
-func HandleUserGet(w http.ResponseWriter, r *http.Request) {
-	user, err := getUserFromSession(r)
-	if err != nil {
-		msg := "Couldn't find user in session"
-		http.Error(w, msg, http.StatusBadRequest)
-		return
-	}
-
-	userJson, err := json.Marshal(user)
-	if err != nil {
-		log.Error("Couldn't marshal user json: %v", err)
-		http.Error(w, INTERNAL_SERVER_ERROR_MSG, http.StatusInternalServerError)
-		return
-	}
-
-	fmt.Fprint(w, string(userJson))
 }
 
 func HandleGetAllArticleNames(w http.ResponseWriter, r *http.Request) {
